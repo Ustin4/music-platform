ddeab966a3b0b0246378318a543989fbe8e6bb47
Ustin4 <Mortyvp@yandex.by>
2024-10-13 22:15:39 +0300

    add store

---
+++ client/store/reducers/playerReducer.ts
@@ -0,0 +1,9 @@
+import {PlayerActionTypes, PlayerInterface} from "@/types/player";
+
+const initialState:PlayerInterface = {
+
+}
+
+export const playerReducer = (state = initialState,action:PlayerActionTypes):PlayerInterface=>{
+
+}
